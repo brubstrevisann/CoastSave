826de445a7c01b5e7dd682771149410b7fa234f4
pequebego <pequebego10@gmail.com>
2019-10-20 16:32:46 -0300

    camera/mapa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+ 
+  imagem: any = null;
 
-  constructor() {}
+  private options: CameraOptions = {
+    quality: 50,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+  }
+  constructor(private camera: Camera ,private router:  Router) { }
 
+  ngOnInit() {
+  }
+  baterFoto() {
+    this.camera.getPicture(this.options).then((imageData) => {
+      this.imagem = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      alert('Ops!\nHouve um erro');
+      console.log(err)
+    });
+  }
+ 
 }
